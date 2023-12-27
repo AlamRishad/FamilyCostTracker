077dc44d3eab8c0ae8a5c6e32ce07e8aaedbc22d
Mahfuzul Alam <mahfuzulalamrishad@gmail.com>
2023-12-27 18:28:23 +0600

    enable screen added

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 import Main from "./AppContainer";
 export default function App() {
   return (
     <View style={styles.container}>
-			<Main />
-
-			<StatusBar style="auto" />
-		</View>
+      <Main />
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-		height: "100%",
+    height: "100%",
   },
 });
--- AppContainer/index.js
+++ AppContainer/index.js
@@ -1,23 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { Dimensions, View } from "react-native";
 import AppStackNavigator from "./Components/Navigators/AppStackNavigator";
+import { enableScreens } from "react-native-screens";
+enableScreens();
+
 const windowHeight = Dimensions.get("window").height;
 
 export default function Index() {
-
-	
-
-	return (
-		<View  style={{ height: windowHeight + 30 }}>
-			
-			
-				<NavigationContainer>
-
-						<AppStackNavigator />
-					
-					
-				</NavigationContainer>
-			
-		</View>
-	);
+  return (
+    <View style={{ height: windowHeight + 30 }}>
+      <NavigationContainer>
+        <AppStackNavigator />
+      </NavigationContainer>
+    </View>
+  );
 }
